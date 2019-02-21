306e50b2088ee0f638e03218317f4576c947bee3
Virginie <bonnefond.virginie@gmail.com>
2019-02-21 20:49:43 +0000

    Add Jaccard Index to Evaluation Metrics

--- tools/generic_dl_tools/performance_metrics.py
+++ tools/generic_dl_tools/performance_metrics.py
@@ -1,2 +1,13 @@
-def function(x):
-    return x
+class EvaluationMetrics(object):
+
+    def jaccard_index(self, y_true, y_pred):
+        """Implementation of intersection over union method for binary arrays.
+        :param y_true: numpy.array
+        :param y_pred: numpy.array
+        :rtype: float
+        """
+        y_true = y_true.astype(int)
+        y_pred = y_pred.astype(int)
+        area_of_intersection = sum((y_true & y_pred).flatten())
+        area_of_union = sum((y_true | y_pred).flatten())
+        return float(area_of_intersection) / area_of_union
